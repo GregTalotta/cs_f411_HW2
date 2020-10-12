6f04e393898d894e0916a3a9db5f22cb834900f0
Gregory Talotta <gjtalotta@alaska.edu>
2020-10-11 17:48:47 -0800

    the first way works

--- build.cpp
+++ build.cpp
@@ -1,6 +1,6 @@
 // build.cpp
 // Greg Talotta
-// 11 Oct 2020
+// 28 Sep 2020
 //
 // For CS 411 Fall 2020
 // Used in Assignment 2, Exercise A
@@ -10,35 +10,60 @@
 int build(int w, int e, const vector<Bridge> &bridges)
 {
     int max_toll = 0;
+    vector<int> totals;
     vector<Bridge> set;
-    build_count(bridges, 0, set, 0, max_toll);
+    build_count(bridges, 0, set, totals);
+    for (int i = 0; i < totals.size(); ++i)
+    {
+        if (totals[i] > max_toll)
+        {
+            max_toll = totals[i];
+        }
+    }
     return max_toll;
 }
 
-void build_count(const vector<Bridge> &bridges, int index, vector<Bridge> &bridge_set, int curr_total, int &max_total)
+void build_count(const vector<Bridge> bridges, int index, vector<Bridge> bridge_set, vector<int> &add_totals)
 {
     if (index >= (bridges.size()))
     {
-        if (curr_total > max_total)
-        {
-            max_total = curr_total;
-        }
+        add_totals.push_back(add_toll(bridge_set));
         return;
     }
-    build_count(bridges, index + 1, bridge_set, curr_total, max_total);
-    if (!cross_set(bridges[index], bridge_set))
+    if (cross_set(bridges[index], bridge_set))
+    {
+        build_count(bridges, (index + 1), bridge_set, add_totals);
+        vector<int> indecies;
+        for (int i = 0; i < bridge_set.size(); ++i)
+        {
+            if (cross(bridges[index], bridge_set[i]))
+            {
+                indecies.push_back(i);
+            }
+        }
+        vector<Bridge> new_bridge_set;
+        for (int i = 0; i < bridge_set.size(); ++i)
+        {
+            if (std::find(indecies.begin(), indecies.end(), i) == indecies.end())
+            {
+                new_bridge_set.push_back(bridge_set[i]);
+            }
+        }
+        new_bridge_set.push_back(bridges[index]);
+        build_count(bridges, (index + 1), new_bridge_set, add_totals);
+    }
+    else
     {
         bridge_set.push_back(bridges[index]);
-        build_count(bridges, index + 1, bridge_set, (curr_total + bridges[index][2]), max_total);
-        bridge_set.pop_back();
+        build_count(bridges, (index + 1), bridge_set, add_totals);
     }
 }
 
 bool cross_set(Bridge test, vector<Bridge> &against)
 {
-    for (auto i : against)
+    for (int i = 0; i < against.size(); ++i)
     {
-        if (cross(test, i))
+        if (cross(test, against[i]))
         {
             return true;
         }
@@ -46,11 +71,21 @@ bool cross_set(Bridge test, vector<Bridge> &against)
     return false;
 }
 
-bool cross(Bridge &test, Bridge &against)
+bool cross(Bridge test, Bridge against)
 {
     if ((test[0] > against[0]) && (test[1] < against[1]) || (test[0] < against[0]) && (test[1] > against[1]) || (test[0] == against[0]) || (test[1] == against[1]))
     {
         return true;
     }
     return false;
+}
+
+int add_toll(vector<Bridge> set)
+{
+    int total = 0;
+    for (int i = 0; i < set.size(); ++i)
+    {
+        total += set[i][2];
+    }
+    return total;
 }
--- build.h
+++ build.h
@@ -1,6 +1,6 @@
 // build.h
 // Greg Talotta
-// 11 Oct 2020
+// 28 Sep 2020
 //
 // For CS 411 Fall 2020
 // Used in Assignment 2, Exercise A
@@ -12,8 +12,9 @@ using std::vector;
 using Bridge = vector<int>;
 
 int build(int w, int e, const vector<Bridge> &bridges);
-void build_count(const vector<Bridge> &bridges, int index, vector<Bridge> &bridge_set, int curr_total, int &max_total);
+void build_count(const vector<Bridge> bridges, int index, vector<Bridge> bridge_set, vector<int> &add_totals);
 bool cross_set(Bridge test, vector<Bridge> &against);
-bool cross(Bridge &test, Bridge &against);
+bool cross(Bridge test, Bridge against);
+int add_toll(vector<Bridge> set);
 
 #endif //#ifndef FILE_BUILD_H_INCLUDED
